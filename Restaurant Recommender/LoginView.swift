4a2c27438f9071f3ac70a492c419701fa06ab158
Jaxcho <jaxcho1118@gmail.com>
2026-05-22 11:33:11 -0700

    idek

---
+++ Restaurant Recommender/LoginView.swift
@@ -0,0 +1,66 @@
+//
+//  LoginView.swift
+//  Restaurant Recommender
+//
+//  Created by Jax Choi on 5/17/26.
+//
+
+import SwiftUI
+
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    func login(){
+        if email == "" || password == "" {
+            return
+        }
+        var request: URLRequest = URLRequest(url: URL(string: "http://192.168.86.48:8000/auth/login")!)
+        //        var request: URLRequest = URLRequest(url: URL(string: "http://127.0.0.1:8000/auth/login")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let formFields: [[String: Any]] = [
+            [
+            "id": "email",
+            "value": email],
+            [
+                "id": "password",
+                "value": password
+            ]
+        ]
+        
+        let body = [ "formFields": formFields ]
+        
+        let data = try! JSONSerialization.data(withJSONObject: body)
+        request.httpBody = data
+        
+        URLSession.shared.dataTask(with: request, completionHandler: {
+            data, response, error in
+            if data != nil, let json: [String : Any] = try? JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                print("\(data)")
+            } else {
+                print("\(error)")
+            }
+        }).resume()
+//        let bodyData: [String: String] = ["email": email, "password": password]
+//        request.httpBody = try? JSONEncoder().encode(bodyData)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                print(String(data: data, encoding: .utf8)!)
+//            }
+//        }.resume()
+    }
+
+    
+    var body: some View {
+        TextField("Email", text: $email)
+        TextField("Password", text: $password)
+        Button("Submit", action: login)
+        Text("Hello, World!")
+    }
+}
+
+//#Preview {
+////    LoginView()
+//}
