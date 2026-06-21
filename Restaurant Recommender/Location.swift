79679b118c6bcaf336b7aae97658abd8ae9600a6
Jaxcho <jaxcho1118@gmail.com>
2026-06-21 12:52:23 -0700

    sum fixes

--- Restaurant Recommender/Location.swift
+++ Restaurant Recommender/Location.swift
@@ -4,4 +4,47 @@
 //
 //  Created by Jax Choi on 6/20/26.
 //
+import SwiftUI
+import CoreLocation
+import Combine
+
+@MainActor
+class Location: ObservableObject {
+    
+    @Published var location: CLLocation?
+    
+    func startUpdating() async{
+        let updates = CLLocationUpdate.liveUpdates()
+        do{
+            for try await update in updates{
+                if let newLocation = update.location{
+                    self.location = newLocation
+                }
+            }
+        }
+        
+        catch{
+            print("Location failed: \(error)")
+        }
+        
+    }
+}
+
+struct LocationView: View {
+    @StateObject private var Loc = Location()
+    
+    var body: some View {
+        VStack (spacing: 16) {
+            if let location = Loc.location {
+                Text("Latitude: \(location.coordinate.latitude)")
+                Text("Longitude: \(location.coordinate.longitude)")
+            } else {
+                Text("Fetching location...")
+            }
+        }
+        .task {
+            await Loc.startUpdating()
+        }
+    }
+}
 
--- Restaurant Recommender/PostLoginView.swift
+++ Restaurant Recommender/PostLoginView.swift
@@ -4,4 +4,19 @@
 //
 //  Created by Jax Choi on 6/20/26.
 //
+import SwiftUI
 
+struct PostLoginView: View {
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Text("Welcome!")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PostLoginView()
+}
